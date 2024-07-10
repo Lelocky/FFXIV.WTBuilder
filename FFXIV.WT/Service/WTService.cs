f5e9f02b39638fc2b933b6cd32edf32b94bd32f9
Tim Kroon <tim@tkroon.nl>
2024-07-10 22:06:05 +0200

    Change styling

--- FFXIV.WT/Service/WTService.cs
+++ FFXIV.WT/Service/WTService.cs
@@ -1,7 +1,12 @@
 ﻿using FFXIV.WT.Model;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FFXIV.WT.Service
@@ -11,10 +16,13 @@ namespace FFXIV.WT.Service
         private Dictionary<Guid, Duty> Duties;
         private Dictionary<Guid, Party> Parties;
         public event Action OnChange;
-        public WTService()
+        private readonly ILogger<WTService> _logger;
+        
+        public WTService(ILogger<WTService> logger)
         {
             PopulateDuties();
             Parties = new Dictionary<Guid, Party>();
+            _logger = logger;
         }
 
         public Guid AddParty()
@@ -78,112 +86,38 @@ namespace FFXIV.WT.Service
             return true;
         }
 
-        private void PopulateDuties()
+        private async Task PopulateDuties()
         {
             Duties = new Dictionary<Guid, Duty>();
 
-            //Dungeons
-            AddDuty("Dungeons (Lv. 1-49)", 1 );
-            AddDuty("Dungeons (Lv. 50)", 1);
-            AddDuty("Dungeons (Lv. 51-59", 1);
-            AddDuty("Dungeons (Lv. 60)", 1);
-            AddDuty("Dungeons (Lv. 61-69)", 1);
-            AddDuty("Dungeons (Lv. 70)", 1);
-            AddDuty("Dungeons (Lv. 71-79)", 1);
-            AddDuty("Dungeons (Lv. 80)", 1);
+            var remoteDutyList = await GetRemoteDutyList();
 
-            //Raids
-            AddDuty("Labyrinth of the Ancients", 2);
-            AddDuty("Syrcus Tower", 2);
-            AddDuty("The World of Darkness", 2);
-            AddDuty("The Void Ark", 2);
-            AddDuty("The Weeping City of Mhach", 2);
-            AddDuty("Dun Scaith", 2);
-            AddDuty("The Royal City of Rabanastre", 2);
-            AddDuty("The Ridorana Lighthouse", 2);
-            AddDuty("The Orbonne Monastery", 2);
+            foreach (var a in remoteDutyList)
+            {
+                Duties.Add(a.Key, new Duty { Key = a.Key, Category = a.Category, DutyName = a.Name });
+            }
+        }
 
-            AddDuty("The Binding Coil of Bahamut Turn 1", 2);
-            AddDuty("The Binding Coil of Bahamut Turn 2", 2);
-            AddDuty("The Binding Coil of Bahamut Turn 3", 2);
-            AddDuty("The Binding Coil of Bahamut Turn 4", 2);
-            AddDuty("The Binding Coil of Bahamut Turn 5", 2);
+        private async Task<List<RemoteDuty>> GetRemoteDutyList()
+        {
+            try
+            {
+                string downloadUrl = "https://raw.githubusercontent.com/Lelocky/FFXIV.WTBuilder/main/data/duties.json";
 
-            AddDuty("The Second Coil of Bahamut Turn 1", 2);
-            AddDuty("The Second Coil of Bahamut Turn 2", 2);
-            AddDuty("The Second Coil of Bahamut Turn 3", 2);
-            AddDuty("The Second Coil of Bahamut Turn 4", 2);
+                using var client = new HttpClient();
+                using var s = await client.GetStreamAsync(downloadUrl);
+                using var reader = new StreamReader(s);
 
-            AddDuty("The Final Coil of Bahamut Turn 1", 2);
-            AddDuty("The Final Coil of Bahamut Turn 2", 2);
-            AddDuty("The Final Coil of Bahamut Turn 3", 2);
-            AddDuty("The Final Coil of Bahamut Turn 4", 2);
+                var dutyJson = reader.ReadToEnd();
 
-            AddDuty("Alexander - The Fist of the Father", 2);
-            AddDuty("Alexander - The Cuff of the Father", 2);
-            AddDuty("Alexander - The Arm of the Father", 2);
-            AddDuty("Alexander - Burden of the Father", 2);
+                return JsonConvert.DeserializeObject<List<RemoteDuty>>(dutyJson);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Exception while getting remote duties");
+                return new List<RemoteDuty>();
+            }
 
-            AddDuty("Alexander - The Fist of the Son", 2);
-            AddDuty("Alexander - The Cuff of the Son", 2);
-            AddDuty("Alexander - The Arm of the Son", 2);
-            AddDuty("Alexander - Burden of the Son", 2);
-
-            AddDuty("Alexander - The Eyes of the Creator", 2);
-            AddDuty("Alexander - Breath of the Creator", 2);
-            AddDuty("Alexander - Heart or Soul of the Creator", 2);
-            AddDuty("Alexander - Soul of the Creator", 2);
-
-            AddDuty("Omega: Deltascape V1.0", 2);
-            AddDuty("Omega: Deltascape V2.0", 2);
-            AddDuty("Omega: Deltascape V3.0", 2);
-            AddDuty("Omega: Deltascape V4.0", 2);
-
-            AddDuty("Omega: Sigmascape V1.0", 2);
-            AddDuty("Omega: Sigmascape V2.0", 2);
-            AddDuty("Omega: Sigmascape V3.0", 2);
-            AddDuty("Omega: Sigmascape V4.0", 2);
-
-            AddDuty("Omega: Alphascape V1.0", 2);
-            AddDuty("Omega: Alphascape V2.0", 2);
-            AddDuty("Omega: Alphascape V3.0", 2);
-            AddDuty("Omega: Alphascape V4.0", 2);
-
-            AddDuty("Eden's Gate: Resurrection", 2);
-            AddDuty("Eden's Gate: Inundation", 2);
-            AddDuty("Eden's Gate: Descent", 2);
-            AddDuty("Eden's Gate: Sepulture", 2);
-
-            //Trials
-            AddDuty("The Minstrel's Ballad: Ultima's Bane", 3);
-            AddDuty("The Howling Eye (Extreme)", 3);
-            AddDuty("The Navel (Extreme)", 3);
-            AddDuty("The Bowl of Embers (Extreme)", 3);
-            AddDuty("Thornmarch (Extreme)", 3);
-            AddDuty("The Whorleater (Extreme)", 3);
-            AddDuty("The Striking Tree (Extreme)", 3);
-            AddDuty("Akh Afah Amphitheatre (Extreme)", 3);
-            AddDuty("The Limitless Blue (Extreme)", 3);
-            AddDuty("Thok ast Thok (Extreme)", 3);
-            AddDuty("The Minstrel's Ballad: Thordan's Reign", 3);
-            AddDuty("The Minstrel's Ballad: Nidhogg's Rage", 3);
-            AddDuty("Containment Bay S1T7 (Extreme)", 3);
-            AddDuty("Containment Bay P1T6 (Extreme)", 3);
-            AddDuty("Containment Bay Z1T9 (Extreme)", 3);
-            AddDuty("Emanation (Extreme)", 3);
-            AddDuty("The Pool of Tribute (Extreme)", 3);
-            AddDuty("The Minstrel's Ballad: Shinryu's Domain", 3);
-            AddDuty("The Jade Stoa (Extreme)", 3);
-            AddDuty("The Minstrel's Ballad: Tsukuyomi's Pain", 3);
-            AddDuty("The Great Hunt (Extreme)", 3);
-            AddDuty("Hells' Kier (Extreme)", 3);
-            AddDuty("The Wreath of Snakes (Extreme)", 3);
-
-            //Other
-            AddDuty("Aquapolis/Uznair/Lyhe Ghiah", 4);
-            AddDuty("Palace of the Dead/Heaven-on-High", 4);
-            AddDuty("PvP: Frontlines/Danshig Naadam, Astragalos/Hidden Gorge", 4);
-            AddDuty("The Feast", 4);
         }
 
         public void AddDuty(string name, int category)
@@ -208,5 +142,12 @@ namespace FFXIV.WT.Service
         }
 
         private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+
+    public class RemoteDuty
+    {
+        public Guid Key { get; set; }
+        public string Name { get; set; }
+        public int Category { get; set; }
     }
 }
